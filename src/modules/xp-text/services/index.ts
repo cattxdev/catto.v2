36784c42b657754a70686511afd5aa723068ab93
A. Mestra <89419093+invaliderrors@users.noreply.github.com>
2026-01-18 20:49:28 -0500

    feat(xp-system): implement XP configuration, user XP management, and leaderboard services

---
+++ src/modules/xp-text/services/index.ts
@@ -0,0 +1,8 @@
+/**
+ * XP System Services
+ */
+
+export * as configService from './xp-text-config.service';
+export * as levelService from './xp-text-level.service';
+export * as awardService from './xp-text-award.service';
+export * as leaderboardService from './xp-text-leaderboard.service';
