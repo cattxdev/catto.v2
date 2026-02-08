8cdc0d8bd735c7fa374f20a805d59341e1e43136
mateoltd <mateo@auraxgroup.com>
2026-02-08 11:57:33 +0100

    chore: bump to latest nextjs version
    im not deploying with multiple CVE to the internet

--- dashboard/eslint.config.mjs
+++ dashboard/eslint.config.mjs
@@ -1,5 +1,6 @@
-export default [
-  {
-    ignores: ['.next/**', 'node_modules/**', 'dist/**'],
-  },
-];
+import next from "eslint-config-next";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+export default [...next, ...nextCoreWebVitals, ...nextTypescript, {
+  ignores: ['.next/**', 'node_modules/**', 'dist/**'],
+}];
